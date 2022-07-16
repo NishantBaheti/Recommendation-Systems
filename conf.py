6fb0d03c8272bb2eeeb50143af901499f0bd5969
Nishant Baheti <nishantbaheti.it19@gmail.com>
2022-07-17 02:14:30 +0530

    progress commit

--- conf.py
+++ conf.py
@@ -44,6 +44,8 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', '**.ipynb_checkpoints', 'docs']
 
 
+
+html_title = "Recommendation-Systems"
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
